0772bde78b1dbd83b158ff46bda6154b46f0fc4e
emnkvk <emin.kvk.929@gmail.com>
2022-11-22 12:31:35 +0300

    Preparing---> CucumberFrameWork

---
+++ src/test/java/StepDefinitions/_01_LoginSteps.java
@@ -0,0 +1,19 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class _01_LoginSteps {
+    @Given("Navigate to Campus")
+    public void navigateToCampus() {
+    }
+
+    @When("Enter username and password after click Login button")
+    public void enterUsernameAndPasswordAfterClickLoginButton() {
+    }
+
+    @Then("User should login successfuly")
+    public void userShouldLoginSuccessfuly() {
+    }
+}
