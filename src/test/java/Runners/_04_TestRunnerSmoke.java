6bb9737af806973b4f368d625c03f2a5fb59be81
emnkvk <emin.kvk.929@gmail.com>
2023-01-17 10:42:27 +0300

    Jenkins Settings. PLugin

--- src/test/java/Runners/_04_TestRunnerSmoke.java
+++ src/test/java/Runners/_04_TestRunnerSmoke.java
@@ -6,8 +6,9 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         tags = "@SmokeTest", // hangi senaryolarda bu etiket varsa onlar çalıştırılacak
         features = {"src/test/java/FeatureFiles/"},
-        glue = {"StepDefinitions"}
+        glue = {"StepDefinitions"},
         //plugin = { "html:target//cucumber-reports.html" }
+        plugin = {"pretty","html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json"}
 )
 
 public class _04_TestRunnerSmoke extends AbstractTestNGCucumberTests {
